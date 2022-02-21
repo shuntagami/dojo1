52e37b95b0b4fbb4579605597e18870690382c54
shuntagami <shun.tagami@i-nations.tokyo>
2022-02-21 22:30:53 +0900

    kadai1: Used groutine for converting file

--- kadai1/shuntagami/converter/converter.go
+++ kadai1/shuntagami/converter/converter.go
@@ -1,6 +1,7 @@
 package converter
 
 import (
+	"fmt"
 	"image"
 	"image/jpeg"
 	"image/png"
@@ -9,6 +10,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 
 	"github.com/shuntagami/dojo1/kadai1/shuntagami/helper"
 	"github.com/shuntagami/dojo1/kadai1/shuntagami/validator"
@@ -53,62 +55,81 @@ func (c *ConverterClient) Convert(targetDirName string) error {
 	}
 
 	// ターゲットディレクトリ配下の画像ファイルをループして形式変換する
+	ch := make(chan string)
+	var wg sync.WaitGroup
+
 	for _, file := range files {
 		if !file.IsDir() {
 			extension := strings.ToLower(filepath.Ext(file.Name()))
 			name := file.Name()[0 : len(file.Name())-len(extension)]
 			if extension == c.From {
-				target, err := os.Open(filepath.Join(pathToTargetDir, file.Name()))
+				wg.Add(1)
+
+				pathToTargetFile := filepath.Join(pathToTargetDir, file.Name())
+				targetFile, err := os.Open(pathToTargetFile)
 				if err != nil {
 					return err
 				}
-				defer target.Close()
+				defer targetFile.Close()
 
 				// converted配下にPROJECT_ROOT_DIR名を含める必要ないためカットする
-				destpath := pathToTargetDir[len(c.RootDir):]
+				destDir := filepath.Join("./converted/", pathToTargetDir[len(c.RootDir):])
+				pathToDestFile := filepath.Join(destDir, name+c.To)
 
 				// ディレクトリ作成
-				if err := os.MkdirAll(filepath.Join("./converted/", destpath), os.ModePerm); err != nil {
+				if err := os.MkdirAll(destDir, os.ModePerm); err != nil {
 					return err
 				}
 
 				// 変換後のファイルを作成
-				dest, err := os.Create(filepath.Join("./converted/", destpath, name+c.To))
+				destFile, err := os.Create(pathToDestFile)
 				if err != nil {
 					return err
 				}
 
 				// 画像ファイルの変換を実行
-				if err := convertSingleIMGFile(c.From, dest, target); err != nil {
-					return err
-				}
+				go c.convertSingleIMGFile(pathToTargetFile, pathToDestFile, destFile, targetFile, ch, &wg)
 			}
 		} else {
 			// ターゲットディレクトリ配下にさらにディレクトリがあった場合は再起的に変換処理を実行する
 			c.Convert(file.Name())
 		}
 	}
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	for msg := range ch {
+		fmt.Println(msg)
+	}
 	return nil
 }
 
 // convertSingleIMGFile converts a single img file to jpg or png
-func convertSingleIMGFile(from string, w io.Writer, r io.Reader) error {
+func (c *ConverterClient) convertSingleIMGFile(target, dest string, w io.Writer, r io.Reader, ch chan string, wg *sync.WaitGroup) {
+	defer (*wg).Done()
+
 	var img image.Image
 	var err error
 
-	switch from {
+	switch c.From {
 	case helper.JPG:
 		img, err = jpeg.Decode(r)
-		if err != nil {
-			return err
+		if err == nil {
+			err = png.Encode(w, img)
 		}
-		err = png.Encode(w, img)
 	case helper.PNG:
 		img, err = png.Decode(r)
-		if err != nil {
-			return err
+		if err == nil {
+			err = jpeg.Encode(w, img, nil)
 		}
-		err = jpeg.Encode(w, img, nil)
 	}
-	return err
+
+	if err == nil {
+		ch <- fmt.Sprintf("Successfully converted %s, to %s", target, dest)
+	} else {
+		ch <- fmt.Sprintf("Failed to convert %s, to %s: %s", target, dest, err.Error())
+	}
 }
